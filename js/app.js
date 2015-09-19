463daa09589da99af869a65d6891dd16243a7f0a
Jason Thomas <nanoman689@gmail.com>
2015-09-18 19:33:19 -0700

    Starting to add animations.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,47 @@
 angular.module('myApp', ['ngAnimate'])
+/*
 .config(function($httpProvider) {
 	$httpProvider.defaults.useXDomain = true;
 })
+*/
 
 .controller('myController', function($scope, $http, $sce) {
 
+	$scope.searchInstaGram = function(keyword){
+
+		$scope.keyword = keyword;
+		var url = "https://api.instagram.com/v1/tags/" + keyword + "/media/recent"  +"?callback=JSON_CALLBACK&client_id=97a493656416448891fba171c5f9747d";
+
+		$http.jsonp(url)
+ 			.success(function(response) {
+				console.log(response.data);
+				$scope.results = response.data;
+
+    })
+			.error(function(error) {
+			console.log("error");
+
+			});
 
 
+
+/*
+		$http({
+			method: 'GET',
+			url: url,
+			params: request
+		})
+		.then(function (response) {
+			console.log(response.data);
+			$scope.results = response.data;
+		},
+		function(response) {
+			alert('error');
+		});
+*/
+
+	};
+});
 
 /*
 
@@ -28,5 +63,8 @@ CLIENT SECRET	da103b22e88f4da9b234c3dbdd53a828
 WEBSITE URL	http://www.basement9.com
 REDIRECT URI	http://www.basement9.com
 SUPPORT EMAIL	nanoman689@gmail.com
+
+
+images.standard_resolution.URL
 
 */
