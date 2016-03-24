f14ed6eb28f28330d1ee3d7f457de8952cbeb877
Jason Thomas <nanoman689@gmail.com>
2016-03-23 19:09:03 -0700

    03-23-16

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # instagramSearch
+
+A Thinkful.com project by Jason M. Thomas.
+
+Using the Instagram API, pull images from a searach and display them on the screen.
--- js/app.js
+++ js/app.js
@@ -6,11 +6,17 @@ angular.module('myApp', ['ngAnimate'])
 */
 
 .controller('myController', function($scope, $http, $sce) {
-
-	$scope.searchInstaGram = function(keyword){
-
-		$scope.keyword = keyword;
-		var url = "https://api.instagram.com/v1/tags/" + keyword + "/media/recent"  +"?callback=JSON_CALLBACK&client_id=97a493656416448891fba171c5f9747d";
+    $scope.keyword="cats";
+    
+    $scope.clearSearch = function(){
+            $scope.keyword = "";
+            $scope.results = [];
+            console.log('reset!');
+    };
+    
+	$scope.searchInstaGram = function(){
+        
+		var url = "https://api.instagram.com/v1/tags/" + $scope.keyword + "/media/recent"  +"?callback=JSON_CALLBACK&client_id=97a493656416448891fba171c5f9747d";
 
 		$http.jsonp(url)
  			.success(function(response) {
@@ -23,48 +29,8 @@ angular.module('myApp', ['ngAnimate'])
 
 			});
 
-
-
-/*
-		$http({
-			method: 'GET',
-			url: url,
-			params: request
-		})
-		.then(function (response) {
-			console.log(response.data);
-			$scope.results = response.data;
-		},
-		function(response) {
-			alert('error');
-		});
-*/
-
 	};
+    
+    $scope.searchInstaGram();
+    
 });
-
-/*
-
-https://api.instagram.com/v1/tags/{tag}/media/recent
-
-callback: 'JSON_CALLBACK'
-client_id: 'your_client_id'
-
-user authorization url
-
-https://api.instagram.com/oauth/authorize/?client_id=CLIENT-ID&redirect_uri=REDIRECT-URI&response_type=code
-
-Receive the redirect from Instagram
-
-http://your-redirect-uri?code=CODE
-
-CLIENT ID	97a493656416448891fba171c5f9747d
-CLIENT SECRET	da103b22e88f4da9b234c3dbdd53a828
-WEBSITE URL	http://www.basement9.com
-REDIRECT URI	http://www.basement9.com
-SUPPORT EMAIL	nanoman689@gmail.com
-
-
-images.standard_resolution.URL
-
-*/
